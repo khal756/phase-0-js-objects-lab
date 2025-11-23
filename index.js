ad1cc702cb406f34d712d75f877e6dcc89859bfd
ratib khalfah <omondiratib@gmail.com>
2025-11-23 03:23:24 +0300

    Completed Managing Event Attendees Lab

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-//Write your code here
+// Declare and Initialize an Attendee Object
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+// Log Attendee Name
+function logAttendeeName(attendeeObj) {
+  console.log(attendeeObj.name);
+}
 
+// Log Ticket Price
+function logTicketPrice(attendeeObj) {
+  console.log(attendeeObj.ticketPrice);
+}
 
-//Needed for the tests to work. Don't modify
+//  Update Ticket Type
+function updateTicketType(attendeeObj, newType) {
+  attendeeObj.ticketType = newType;
+}
+
+// Update Ticket Price
+function updateTicketPrice(attendeeObj, newPrice) {
+  attendeeObj.ticketPrice = newPrice;
+}
+
+// Remove the Event Property
+function removeEventProperty(attendeeObj) {
+  delete attendeeObj.event;
+}
+
+// Add a Checked-in Property
+function addCheckedInProperty(attendeeObj) {
+  attendeeObj.checkedIn = true;
+}
+
+// Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
